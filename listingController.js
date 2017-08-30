d04f22bcc9e471839552d6decb708f269f7b3ed7
shin <shnnagai@gmail.com>
2017-08-30 17:02:27 -0400

    update all my files

--- listingController.js
+++ listingController.js
@@ -1,14 +1,52 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({
+        "code": $scope.input.code,
+        "name": $scope.input.BuildingName,
+        "coordinates": {
+          "latitude": $scope.input.Latitude,
+          "longitude":$scope.input.Longitude
+        },
+        "address" : $scope.input.Address
+      })
+    };
+    $scope.deleteListing = function(index) {
+       $scope.listings.splice(index, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detaile = $scope.listings[index];
+    };
+
+    $scope.open = function() {
+      $scope.showModal = true;
+    };
+
+    $scope.ok = function() {
+      $scope.showModal = false;
+      $scope.clearData();
+    };
+
+    $scope.cancel = function() {
+      $scope.showModal = false;
+      $scope.clearData();
+    };
+
+    $scope.clearData = function(){
+      $scope.input.code = null;
+      $scope.input.BuildingName = null;
+      $scope.input.Address = null;
+      $scope.input.Latitude = "";
+      $scope.input.Longitude = "";
+    }
   }
 ]);
+
