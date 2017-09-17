443db75f4c2f3b03167de8128c0683cf4a664aae
“YOUR <shnnagai@gmail.com>
2017-09-17 15:34:12 -0400

    fixed the bug on map

--- client/app/config/listings.client.routes.js
+++ client/app/config/listings.client.routes.js
@@ -28,11 +28,11 @@ angular.module('listings').config(['$stateProvider', '$urlRouterProvider',
 
       .state('listings.edit', {
         url: '/edit/:listingId',
-        templateUrl: 'app/view/edit-listing.client.view.html'
+        templateUrl: 'app/views/edit-listing.client.view.html'
       })
       .state('listings.map',{
         url: '/map/',
-        templateUrl: 'app/view/map-listings.client.view.html'
+        templateUrl: 'app/views/map-listings.client.view.html'
       })
   } 
 ]);
